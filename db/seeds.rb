269b2919dc832376ae3e05240e62de36bbb088e4
Andrew <andrew.fillmore@gmail.com>
2021-05-12 14:31:42 -0500

    created new updated seeds file with seed:dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# Product.create(name: "Andrew_photo_one", price: 300, image_url: "Andrew_one", description: "Digital Inkjet Print")
-
-# Product.create(name: "Andrew_photo_two", price: 300, image_url: "Andrew_two", description: "Digital Inkjet Print")
-
-# Product.create(name: "Andrew_photo_three", price: 300, image_url: "Andrew_three", description: "Digital Inkjet Print")
-
-# Product.create(name: "Andrew_photo_four", price: 300, image_url: "Andrew_four", description: "Digital Inkjet Print")
-
-# Product.create(name: "Andrew_photo_five", price: 300, image_url: "Andrew_five", description: "Digital Inkjet Print")
-
-# Product.create(name: "Andrew_sculpture_one", price: 300, image_url: "Andrew_sculpture_one", description: "Wood" )
+Product.create!([
+  {name: "Dior", price: "300.0", image_url: "https://secureservercdn.net/45.40.145.201/7ge.8d4.myftpupload.com/wp-content/uploads/2021/01/3827._Border-937x1024.jpg", description: "Christian Dior Handbag", total_inventory: nil},
+  {name: "Louis Vuitton", price: "900.0", image_url: "https://secureservercdn.net/45.40.145.201/7ge.8d4.myftpupload.com/wp-content/uploads/2021/01/LVPatent-2.jpg", description: "Louis Vuitton Patent Collection", total_inventory: nil},
+  {name: "Christian Louboutin", price: "775.0", image_url: "https://secureservercdn.net/45.40.145.201/7ge.8d4.myftpupload.com/wp-content/uploads/2021/01/signature_3.jpg", description: "Pump", total_inventory: nil},
+  {name: "YSL", price: "700.0", image_url: "https://secureservercdn.net/45.40.145.201/7ge.8d4.myftpupload.com/wp-content/uploads/2021/01/ysl_2.jpg", description: "sandal", total_inventory: nil},
+  {name: "Celine", price: "700.0", image_url: "https://secureservercdn.net/45.40.145.201/7ge.8d4.myftpupload.com/wp-content/uploads/2021/01/0156_2.jpg", description: "Handbag", total_inventory: nil},
+  {name: "Hermes", price: "400.0", image_url: "https://secureservercdn.net/45.40.145.201/7ge.8d4.myftpupload.com/wp-content/uploads/2021/01/2717.jpg", description: "Silk Scarf Tri-tone", total_inventory: nil},
+  {name: "Tom Ford ", price: "800.0", image_url: "https://secureservercdn.net/45.40.145.201/7ge.8d4.myftpupload.com/wp-content/uploads/2021/01/1590.jpg", description: "Trench", total_inventory: nil},
+  {name: "Celine", price: "600.0", image_url: "https://secureservercdn.net/45.40.145.201/7ge.8d4.myftpupload.com/wp-content/uploads/2021/01/0250.jpg", description: "Handbag - red/cream", total_inventory: nil}
+])
