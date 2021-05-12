f1e437bdc136736dc298a6f71d0ef22296dd1e56
Andrew <andrew.fillmore@gmail.com>
2021-05-12 13:57:29 -0500

    Adds total_inventory column to products.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Product.create(name: "Andrew_photo_one", price: 300, image_url: "Andrew_one", description: "Digital Inkjet Print")
+# Product.create(name: "Andrew_photo_one", price: 300, image_url: "Andrew_one", description: "Digital Inkjet Print")
 
-Product.create(name: "Andrew_photo_two", price: 300, image_url: "Andrew_two", description: "Digital Inkjet Print")
+# Product.create(name: "Andrew_photo_two", price: 300, image_url: "Andrew_two", description: "Digital Inkjet Print")
 
-Product.create(name: "Andrew_photo_three", price: 300, image_url: "Andrew_three", description: "Digital Inkjet Print")
+# Product.create(name: "Andrew_photo_three", price: 300, image_url: "Andrew_three", description: "Digital Inkjet Print")
 
-Product.create(name: "Andrew_photo_four", price: 300, image_url: "Andrew_four", description: "Digital Inkjet Print")
+# Product.create(name: "Andrew_photo_four", price: 300, image_url: "Andrew_four", description: "Digital Inkjet Print")
 
-Product.create(name: "Andrew_photo_five", price: 300, image_url: "Andrew_five", description: "Digital Inkjet Print")
+# Product.create(name: "Andrew_photo_five", price: 300, image_url: "Andrew_five", description: "Digital Inkjet Print")
 
-Product.create(name: "Andrew_sculpture_one", price: 300, image_url: "Andrew_sculpture_one", description: "Wood" )
+# Product.create(name: "Andrew_sculpture_one", price: 300, image_url: "Andrew_sculpture_one", description: "Wood" )
