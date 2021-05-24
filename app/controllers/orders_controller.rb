be0253ebf8638c215711b42136c0820a77aaf974
Andrew <andrew.fillmore@gmail.com>
2021-05-24 10:17:31 -0500

    Adds current_user to orders controller create method.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,20 +6,24 @@ class OrdersController < ApplicationController
   end
 
   def create
-    order = Order.new(
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      user_id: current_user.id
-    )
-    # subtotal is products price times orders quantity
-    order.subtotal = order.quantity * order.product.price
-    order.tax = order.subtotal * 0.09
-    order.total = order.tax + order.subtotal
-    
-    if order.save
-      render json: order
+    if current_user
+      order = Order.new(
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        user_id: current_user.id
+      )
+      # subtotal is products price times orders quantity
+      order.subtotal = order.quantity * order.product.price
+      order.tax = order.subtotal * 0.09
+      order.total = order.tax + order.subtotal
+      
+      if order.save
+        render json: order
+      else
+        render json: {errors: order.errors.full_messages}, status: :unprocessable_entity
+      end
     else
-      render json: {errors: order.errors.full_messages}, status: :unprocessable_entity
+      render json: {message: "You must be logged in to do that"}, status: :unauthorized
     end
   end
 end
